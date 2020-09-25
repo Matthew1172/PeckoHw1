9c6a3f66d24bdec43fb918fe11b97ad4aa34a46d
john <js500500506@gmail.com>
2020-09-25 15:13:05 -0400

    Finished

--- PeckoHw1/PeckoHw1.c
+++ PeckoHw1/PeckoHw1.c
@@ -4,15 +4,15 @@
 
 /* mergesort: sort integer array a[length] ; standard mergesort */
 /* sorts in increasing order, requires auxiliary array */
-void mergesort(int* a, int* aux, int length)
+void b_mergesort(int* a, int* aux, int length)
 {
     int i;
     int j1;
     int j2;
     if (length > 1)
     {
-        mergesort(a, aux, (length / 2)); /* recursive calls */
-        mergesort(a + (length / 2), aux, (length - (length / 2)));
+        b_mergesort(a, aux, (length / 2)); /* recursive calls */
+        b_mergesort(a + (length / 2), aux, (length - (length / 2)));
         for (i = 0, j1 = 0, j2 = (length / 2); i < length; ) /* merging */
         {
             if ((j1 < (length / 2) && a[j1] < a[j2]) || j2 == length)
@@ -37,94 +37,25 @@ void pArray(int* a, const int size) {
     for (i = 0; i < size; i++) printf("%d \n", a[i]);
 }
 
-/*
 int common(int* a, int* b, const int as, const int bs) {
-    int c = 0;
-    int i = 0;
-    int j = 0;
-    int z = 0; 
-    int flag = 0;
-    int* bf = (int*)malloc(as * sizeof(int));
-
-    while (i < as) {
-        while (j < bs) {
-            if (a[i] == b[j]) {
-                flag = 0;
-                for (z = 0; z < as; ++z) {
-                    if (bf[z] == a[i]) flag = 1;
-                }
-                if (flag == 0) {
-                    c++;
-                    *(bf + c) = a[i];
-                }
-                i++;
-                j++;
-            }
-            else {
-                if (a[i] < b[j]) i++;
-                else j++;
-            }
-        }
-        break;
-    }
-    return c;
+	int c = 0, i = 0, j = 0;
+	int* au = (int*)malloc(as * sizeof(int));
+	int* bu = (int*)malloc(bs * sizeof(int));
+	int* u = (int*)malloc(bs * sizeof(int));
+	b_mergesort(a, au, as);
+	b_mergesort(b, bu, bs);
+	while(i < as && j < bs){
+		if(a[i] == b[j]){
+			u[c] = a[i];
+			while(a[i] == u[c]) ++i;
+			while(b[j] == u[c]) ++j;
+			c++;
+		}
+		else if(a[i] > b[j]) ++j;
+		else ++i;
+	}
+	return c;
 }
-*/
-
-
-int common(int* a, int* b, const int as, const int bs) {
-    int c = 0;
-    int i = 0;
-    int j = 0;
-    int m = 0;
-
-        while (j < bs || i < as) {
-            if (a[i] != m && a[i] == b[j]) {
-                m = a[i];
-                c++;
-                i++;
-                j++;
-            }
-            else {
-                if (a[i] < b[j]) i++;
-                else j++;
-            }
-        }
-    return c;
-}
-
-
-/*
-int common(int* a, int* b, const int as, const int bs) {
-    int c = 0;
-    int i = 0;
-    int j = 0;
-    int z = 0;
-    int flag = 0;
-    int* bf = (int*)malloc(as * sizeof(int));
-
-    while (i < as || j < bs) {
-            if (a[i] == b[j]) {
-                flag = 0;
-                for (z = 0; z < as; ++z) {
-                    if (bf[z] == a[i]) flag = 1;
-                }
-                if (flag == 0) {
-                    c++;
-                    *(bf + c) = a[i];
-                }
-                i++;
-                j++;
-            }
-            else {
-                if (a[i] < b[j]) i++;
-                else j++;
-            }
-        }
-    pArray(bf, c);
-    return c;
-}
-*/
 
 main()
 {
@@ -132,21 +63,21 @@ main()
     int Q[200000];
     int i, t1, t2, result;
     time_t t;
-    srand((unsigned)time(&t)); /*initialize random numbers */
-    for (i = 0; i < 500000; i++) /* fill P with even numbers */
+    srand((unsigned)time(&t));
+    for (i = 0; i < 500000; i++)
         P[i] = 2 * ((579 * i + 234987) % 500000);
-    for (i = 0; i < 200000; i++)/* fill Q with odd numbers */
+    for (i = 0; i < 200000; i++)
         Q[i] = 2 * ((997 * i + 98771) % 200000) + 1;
     t1 = (rand() % 10) + 1;
     t2 = (rand() % 10) + 1;
-    for (i = 0; i < t1; i++) /* put some odd numbers in P */
+    for (i = 0; i < t1; i++)
     {
         P[rand() % 500000] = 2 * (51 * i + 237) + 1;
         P[rand() % 500000] = 2 * (51 * i + 237) + 1;
         P[rand() % 500000] = 2 * (51 * i + 237) + 1;
         Q[2 * i] = 2 * (51 * i + 237) + 1;
     }
-    for (i = 0; i < t2; i++) /* put some even numbers in Q */
+    for (i = 0; i < t2; i++)
     {
         Q[rand() % 200000] = 2 * (53 * i + 732);
         Q[rand() % 200000] = 2 * (53 * i + 732);
@@ -163,30 +94,3 @@ main()
         printf(" Failed test.\n");
 
 }
-
-
-/*
-int main()
-{
-
-
-    int A[500];
-    int B[500];
-    int C[500];
-    int D[500];
-
-    sArray(A, 500);
-    sArray(C, 500);
-
-    mergesort(A, B, 500);
-    mergesort(C, D, 500);
-    
-    //pArray(A, 500);
-    //pArray(C, 500);
-
-    printf("Common elements: %d \n", common(A, C, 500, 500));
-    
-
-    return 0;
-}
-*/
